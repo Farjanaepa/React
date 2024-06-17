7f240d987841d0060450008d9bd40c659e1a11cd
Farjanaepa <farjanaepa@gmail.com>
2024-06-17 06:53:59 +0600

    first react project

---
+++ React/frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../Components/Header/Header'
+import Exploremenu from '../../Components/ExploreMenu/Exploremenu'
+const Home = () => {
+const [category,setCategory]=useState("All");
+
+
+  return (
+    <div>
+      <Header/>
+      <Exploremenu category={category} setCategory={setCategory}/>
+    </div>
+  )
+}
+
+export default Home
