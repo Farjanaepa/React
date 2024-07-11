9da66dedb14bc849ae3c9755890d187bd0e4d737
Farjanaepa <farjanaepa@gmail.com>
2024-07-11 11:19:30 +0600

    list added

--- React/admin/src/App.jsx
+++ React/admin/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Sidebar from './Components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Order from './Pages/Orders/Order'
+import List from './Pages/List/List'
+import Add from './Pages/Add/Add'
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+
+const url = "http://localhost:4000"
+
   return (
     <div>
-      
+      <ToastContainer/>
+      <Navbar/>
+      <hr />
+      <div className='app-content'>
+        <Sidebar/>
+       <Routes>
+        <Route path='/add' element={<Add url={url}/>} />
+        <Route path='/list' element={<List url={url}/>} />
+        <Route path='/order' element={<Order url={url}/>} />
+       </Routes>
+      </div>
     </div>
   )
 }
