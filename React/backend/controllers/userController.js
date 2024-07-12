eab718986c63d02089f702357bd491df2f1764bd
Farjanaepa <farjanaepa@gmail.com>
2024-07-12 13:38:10 +0600

    login

--- React/backend/controllers/userController.js
+++ React/backend/controllers/userController.js
@@ -6,7 +6,29 @@ import validator from "validator"
 
 //login user
 const loginUser = async (req,res) => {
+    const {email,password} = req.body;
+    try {
+        const user = await userModel.findOne({email});
 
+        if (!user) {
+            return res.json({success:false,message:"User does not exist"})
+            
+        }
+
+      const isMatch = await bcrypt.compare(password,user.password);
+      
+      if (!isMatch) {
+        return res.json({success:false,message:"Invalid credentials"})
+        
+      }
+
+      const token = createToken(user._id);
+      res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
 
 }
 const createToken = (id) => {
@@ -36,7 +58,7 @@ const registerUser = async (req,res) => {
         //hashing user password
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password,salt);
-         const newUser = newModel({
+         const newUser = new userModel({
             name:name,
             email:email,
             password:hashedPassword
