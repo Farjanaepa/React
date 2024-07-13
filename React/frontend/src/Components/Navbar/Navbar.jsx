41f486467bcebda6afc22b68ce0040ac87ede08b
Farjanaepa <farjanaepa@gmail.com>
2024-07-13 19:07:22 +0600

    cart error

--- React/frontend/src/Components/Navbar/Navbar.jsx
+++ React/frontend/src/Components/Navbar/Navbar.jsx
@@ -7,28 +7,54 @@ import { StoreContext } from '../../Context/StoreContext'
 const Navbar = ({setShowLogin}) => {
 
 const [menu,setMenu]= useState('Home')
-const {getTotalCartAmount} = useContext(StoreContext)
+
+
+const {getTotalCartAmount,token,setToken} = useContext(StoreContext)
+
+const logout = () => {
+
+
+}
 
   return (
-    <div className='navbar'>
-   <Link to='/'><img src={assets.logo} alt="" className='logo'/></Link>
-   <ul className="navbar-menu">
-    <Link to='/' onClick={()=>setMenu('Home')} className={menu==='Home'?'active':''}>Home</Link>
-    <a href='#explore-menu' onClick={()=>setMenu('Menu')} className={menu==='Menu'?'active':''}>Menu</a>
-    <a href='#app-download' onClick={()=>setMenu('Mobile-app')} className={menu==='Mobile-app'?'active':''}>Mobile-app</a>
-    <a href='#footer' onClick={()=>setMenu('Contact-us')} className={menu==='Contact-us'?'active':''}>Contact us</a>
-    </ul>
+  <div className='navbar'>
+    <Link to='/'><img src={assets.logo} alt="" className='logo'/></Link>
 
-    <div className="navbar-right">
+      <ul className="navbar-menu">
+      
+          <Link to='/' onClick={()=>setMenu('Home')} className={menu==='Home'?'active':''}>Home</Link>
+          <a href='#explore-menu' onClick={()=>setMenu('Menu')} className={menu==='Menu'?'active':''}>Menu</a>
+          <a href='#app-download' onClick={()=>setMenu('Mobile-app')} className={menu==='Mobile-app'?'active':''}>Mobile-app</a>
+          <a href='#footer' onClick={()=>setMenu('Contact-us')} className={menu==='Contact-us'?'active':''}>Contact us</a>
+      
+      </ul>
+
+      <div className="navbar-right">
 
         <img src={assets.search_icon} alt="" />
-        <div className="navbar-search-icon">
-          <Link to='/cart'> <img src={assets.basket_icon} alt="" /></Link>
-            <div className={getTotalCartAmount()===0?"":"dot"}></div>
-        </div>
-        <button onClick={()=>setShowLogin(true)}>Sign in</button>
-    </div>
-    </div>
+          
+          <div className="navbar-search-icon">
+            <Link to='/cart'> <img src={assets.basket_icon} alt="" /></Link>
+              <div className={getTotalCartAmount()===0?"":"dot"}></div>
+          </div>
+
+
+          {!token?<button onClick={()=>setShowLogin(true)}>Sign in</button>
+          :<div className='navbar-profile'>
+
+        <img src={assets.profile_icon} alt="" />
+
+            <ul className='nav-profile-dropdown'>
+              
+              <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+              <hr />
+              <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+
+            </ul>
+        </div>}
+        
+      </div>
+  </div>
   )
 }
 
